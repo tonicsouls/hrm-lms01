7e6cb5f7d4dced6d784f017da9c001ecb52965af
Darry <darry@example.com>
2025-12-03 22:04:21 -0600

    Fix GitHub Pages deployment - make LMS live

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/hrm-lms01/',
     server: {
         port: 5173,
         proxy: {
